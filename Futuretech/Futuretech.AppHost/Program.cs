b3be54c8113998ef859d2b5ccf4d1a671bd501c9
Florian van Dillen <fvandillen@gmail.com>
2024-04-15 13:39:59 +0200

    Add Airport service.

--- Futuretech/Futuretech.AppHost/Program.cs
+++ Futuretech/Futuretech.AppHost/Program.cs
@@ -1,3 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddDapr();
+
+builder
+    .AddProject<Projects.Futuretech_Services_Airport>("airport")
+    .WithDaprSidecar();
+
 builder.Build().Run();
